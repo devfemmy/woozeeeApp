8f4288b902334c3e775e93a0e3bd014ffa285537
thedeji <emmi2110@gmail.com>
2021-06-22 15:38:43 +0100

    added the level 1 nested comment feature

--- src/screens/User/Common/Comments/index.js
+++ src/screens/User/Common/Comments/index.js
@@ -49,9 +49,10 @@ export default function Comments({ route, navigation }) {
 
   const { currUserData, postItem } = route.params;
 
-  console.log(postItem);
+  const [showReplies, setShowReplies] = useState(false);
 
   const [comments, setComments] = useState([]);
+  const [replies, setReplies] = useState([]);
   const [commentId, setCommentId] = useState('');
 
   const fetchComments = async () => {
@@ -80,20 +81,58 @@ export default function Comments({ route, navigation }) {
       .collection('comments')
       .get();
 
-    // allComments.forEach((snap) => {
-    //   console.log('snap ', snap.id);
-    //   console.log(postItem._id.trim());
-    // });
-    console.log('all comments', allComments);
-
     const _comments = [];
+    console.log(' allcomments is', allComments);
     allComments.forEach((snap) => {
-      let replies = Object.assign(snap._data, { replyId: snap.id });
+      let replies = Object.assign(snap._data, {
+        replyId: snap.id,
+        parentCommentId: snap._ref._documentPath._parts[3],
+      });
       _comments.push(replies);
     });
 
     setComments([..._comments]);
-    console.log('fetched comments is', _comments);
+    // console.log('fetched comments is', _comments);
+  };
+
+  const fetchReplies = async (commentId) => {
+    setShowReplies(!showReplies);
+    // console.log(commentId);
+    const firebaseConfig = {
+      apiKey: 'AIzaSyARWCPqpauNDiveSI26tvmKsyn4p_XNzh8',
+      authDomain: 'woozeee-d7f6c.firebaseapp.com',
+      databaseURL: 'https://woozeee-d7f6c.firebaseio.com',
+      projectId: 'woozeee-d7f6c',
+      storageBucket: 'woozeee-d7f6c.appspot.com',
+      messagingSenderId: '979696525592',
+      appId: '1:979696525592:web:ec27a203184d23e0dcfe6d',
+      measurementId: 'G-XQKMT94R9R',
+    };
+
+    try {
+      if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+
+    const allReplies = await firestore()
+      .collection('entryComments')
+      .doc(postItem._id.trim())
+      .collection('comments')
+      .doc(commentId)
+      .collection('replies')
+      .get();
+
+    const _replies = [];
+    allReplies.forEach((snap) => {
+      let replies = Object.assign(snap._data, { commentId: commentId });
+      _replies.push(replies);
+    });
+
+    setReplies([..._replies]);
+    console.log('fetched replies are', _replies);
   };
 
   useEffect(() => {
@@ -354,75 +393,153 @@ export default function Comments({ route, navigation }) {
   };
 
   // prettier-ignore
-  const Message = ({ userName, message, img, time, replyId }) => useMemo(
-    () => (
-      <View>
+  const Message = ({ userName, message, img, time, replyId, parentCommentId }) => {
+    return useMemo(
+      () => (
         <View>
-          <View
-            style={{
-              flexDirection: 'row',
-              alignItems: 'flex-start',
-              marginTop: 15,
-            }}
-          >
-            <LinearGradient
-              colors={['#043F7C', '#FF5757']}
+          <View>
+            <View
               style={{
-                height: 34,
-                width: 34,
-                borderRadius: 17,
-                alignItems: 'center',
-                justifyContent: 'center',
+                flexDirection: 'row',
+                alignItems: 'flex-start',
+                marginTop: 15,
               }}
             >
-              <Image
-                source={img}
+              <LinearGradient
+                colors={['#043F7C', '#FF5757']}
                 style={{
-                  height: 30,
-                  width: 30,
-                  borderRadius: 15,
-                  borderColor: 'white',
+                  height: 34,
+                  width: 34,
+                  borderRadius: 17,
+                  alignItems: 'center',
+                  justifyContent: 'center',
                 }}
-              />
-            </LinearGradient>
-            <Layout
-              level="4"
-              style={{
-                flex: 1,
-                paddingHorizontal: 10,
-                paddingVertical: 5,
-                borderRadius: 10,
-                marginHorizontal: 5,
-              }}
-            >
-              <Text category="s2" style={{ alignSelf: 'flex-start', marginBottom:2 }}>
-                {userName}
-              </Text>
-              <Text category="p2" style={{ marginBottom:2 }}>{message}</Text>
-              <Moment
-                fromNow
-                element={(momentProps) => (
-                  <Text category="c1" {...momentProps} style={{ fontSize: 10, textAlign:'right' }} />
-                )}
               >
-                {time}
-              </Moment>
-            </Layout>
+                <Image
+                  source={img}
+                  style={{
+                    height: 30,
+                    width: 30,
+                    borderRadius: 15,
+                    borderColor: 'white',
+                  }}
+                />
+              </LinearGradient>
+              <Layout
+                level="4"
+                style={{
+                  flex: 1,
+                  paddingHorizontal: 10,
+                  paddingVertical: 5,
+                  borderRadius: 10,
+                  marginHorizontal: 5,
+                }}
+              >
+                <Text category="s2" style={{ alignSelf: 'flex-start', marginBottom:2 }}>
+                  {userName}
+                </Text>
+                <Text category="p2" style={{ marginBottom:2 }}>{message}</Text>
+                <Moment
+                  fromNow
+                  element={(momentProps) => (
+                    <Text category="c1" {...momentProps} style={{ fontSize: 10, textAlign:'right' }} />
+                  )}
+                >
+                  {time}
+                </Moment>
+              </Layout>
+            </View>
+            <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+              <View style={{ marginVertical: 5, marginLeft: 50 }}>
+                <TouchableOpacity
+                  activeOpacity={0.75}
+                  onPress={() => changeFocus(replyId)}
+                >
+                  <Text category="c1">{t('reply')}</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={{ marginVertical: 5, marginLeft: 20 }}>
+                <TouchableOpacity
+                  activeOpacity={0.75}
+                  onPress={() => fetchReplies(replyId)}
+                >
+                <Text category="c1">View replies</Text> 
+                </TouchableOpacity>
+              </View>
+            </View>
           </View>
-          <View style={{ marginVertical: 5, marginLeft: 50 }}>
+          {replies.length > 0 && parentCommentId == replies[0].commentId ? (
+            replies.map((reply, index) => (
+              <>
+              <View
+                key={index}
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'flex-start',
+                  marginTop: 15,
+                  marginLeft:40
+                }}
+              >
+                <LinearGradient
+                  colors={['#043F7C', '#FF5757']}
+                  style={{
+                    height: 24,
+                    width: 24,
+                    borderRadius: 12,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}
+                >
+                  <Image
+                    source={reply.imgUrl}
+                    style={{
+                      height: 30,
+                      width: 30,
+                      borderRadius: 15,
+                      borderColor: 'white',
+                    }}
+                  />
+                </LinearGradient>
+                <Layout
+                  level="4"
+                  style={{
+                    flex: 1,
+                    paddingHorizontal: 10,
+                    paddingVertical: 5,
+                    borderRadius: 10,
+                    marginHorizontal: 5,
+                  }}
+                >
+                  <Text category="s2" style={{ alignSelf: 'flex-start', marginBottom:2 }}>
+                    {reply.userName}
+                  </Text>
+                  <Text category="p2" style={{ marginBottom:2 }}>{reply.text}</Text>
+                  <Moment
+                    fromNow
+                    element={(momentProps) => (
+                      <Text category="c1" {...momentProps} style={{ fontSize: 10, textAlign:'right' }} />
+                    )}
+                  >
+                    {reply.sentAt}
+                  </Moment>
+                </Layout>
+            </View>
+            <View style={{ marginVertical: 5, marginLeft: 80 }}>
             <TouchableOpacity
               activeOpacity={0.75}
-              onPress={() => changeFocus(replyId)}
+              onPress={() => changeFocus(parentCommentId)}
             >
-              <Text category="c2">{t('reply')}</Text>
+              <Text category="c1">{t('reply')}</Text>
             </TouchableOpacity>
           </View>
+          </>
+            ))
+          ):null}       
         </View>
-      </View>
-    ),
-    [],
-  );
-
+      ),
+      [],
+    );
+  }
   return useMemo(
     () => (
       <Layout level="5" style={{ flex: 1 }}>
@@ -441,7 +558,7 @@ export default function Comments({ route, navigation }) {
               justifyContent: 'flex-end',
             }}
           >
-            {console.log('comments in here is => ', comments)}
+            {/* {console.log('comments in here is => ', comments)} */}
             {comments.length > 0 ? (
               <List
                 style={{
@@ -462,6 +579,7 @@ export default function Comments({ route, navigation }) {
                     message={`${comment.item.text}`}
                     time={`${comment.item.sentAt}`}
                     replyId={comment.item.replyId}
+                    parentCommentId={comment.item.parentCommentId}
                   />
                 )}
                 getItemLayout={(data, index) => ({
