a7c2c73681f0dbaa0e8c4edb0373d7d279343090
thedeji <emmi2110@gmail.com>
2021-10-14 12:39:22 +0100

    caption limits

--- src/components/CustomField/index.js
+++ src/components/CustomField/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import {Input} from '@ui-kitten/components';
+import { Input } from '@ui-kitten/components';
 
 const CustomField = (props) => {
-    return (
-        <Input
-        placeholder={props.placeholder}
-        value={props.value}
-        label= {props.label}
-        multiline = {props.multiline}
-        height = {props.height}
-        onChangeText={props.onChangeText}
-      />
-    )
-}
+  return (
+    <Input
+      placeholder={props.placeholder}
+      value={props.value}
+      label={props.label}
+      multiline={props.multiline}
+      height={props.height}
+      maxLength={100}
+      onChangeText={props.onChangeText}
+    />
+  );
+};
 
 export default CustomField;
