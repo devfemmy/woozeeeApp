b2632b9077f769a8da506e9c52cd3cb7d5f92891
thedeji <emmi2110@gmail.com>
2021-10-20 23:30:20 +0100

    range slider fix

---
+++ src/components/RangeSlider/screens/Slider/index.js
@@ -0,0 +1,104 @@
+import React, { useCallback, useState } from 'react';
+import { View } from 'react-native';
+import { Text } from '@ui-kitten/components';
+import Slider from 'rn-range-slider';
+
+import Thumb from '../../Slider/Thumb';
+import Rail from '../../Slider/Rail';
+import RailSelected from '../../Slider/RailSelected';
+import Notch from '../../Slider/Notch';
+import Label from '../../Slider/Label';
+import TextButton from '../components/TextButton';
+
+import styles from './styles';
+
+const SliderScreen = () => {
+  const [rangeDisabled, setRangeDisabled] = useState(false);
+  const [low, setLow] = useState(0);
+  const [high, setHigh] = useState(100);
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(100);
+  const [floatingLabel, setFloatingLabel] = useState(false);
+
+  const renderThumb = useCallback(() => <Thumb />, []);
+  const renderRail = useCallback(() => <Rail />, []);
+  const renderRailSelected = useCallback(() => <RailSelected />, []);
+  const renderLabel = useCallback((value) => <Label text={value} />, []);
+  const renderNotch = useCallback(() => <Notch />, []);
+  const handleValueChange = useCallback((low, high) => {
+    setLow(low);
+    setHigh(high);
+  }, []);
+  const toggleRangeEnabled = useCallback(
+    () => setRangeDisabled(!rangeDisabled),
+    [rangeDisabled],
+  );
+  const setMinTo50 = useCallback(() => setMin(50), []);
+  const setMinTo0 = useCallback(() => setMin(0), []);
+  const setMaxTo100 = useCallback(() => setMax(100), []);
+  const setMaxTo500 = useCallback(() => setMax(500), []);
+  const toggleFloatingLabel = useCallback(
+    () => setFloatingLabel(!floatingLabel),
+    [floatingLabel],
+  );
+
+  return (
+    <View style={styles.root}>
+      <Text status="basic">
+        {low}% - {high}%
+      </Text>
+      <Slider
+        style={styles.slider}
+        min={min}
+        max={max}
+        step={1}
+        disableRange={rangeDisabled}
+        floatingLabel={floatingLabel}
+        renderThumb={renderThumb}
+        renderRail={renderRail}
+        renderRailSelected={renderRailSelected}
+        renderLabel={renderLabel}
+        renderNotch={renderNotch}
+        onValueChanged={handleValueChange}
+      />
+      {/* <View style={styles.horizontalContainer}>
+      <TextButton
+        text="Toggle floating"
+        containerStyle={styles.button}
+        onPress={toggleFloatingLabel}
+      />
+      <TextButton
+        text={rangeDisabled ? 'Enable range' : 'Disable range'}
+        containerStyle={styles.button}
+        onPress={toggleRangeEnabled}
+      />
+    </View>
+    <View style={styles.horizontalContainer}>
+      <TextButton
+        text="Set min to 0"
+        containerStyle={styles.button}
+        onPress={setMinTo0}
+      />
+      <TextButton
+        text="Set min to 50"
+        containerStyle={styles.button}
+        onPress={setMinTo50}
+      />
+    </View>
+    <View style={styles.horizontalContainer}>
+      <TextButton
+        text="Set max to 100"
+        containerStyle={styles.button}
+        onPress={setMaxTo100}
+      />
+      <TextButton
+        text="Set max to 500"
+        containerStyle={styles.button}
+        onPress={setMaxTo500}
+      />
+    </View> */}
+    </View>
+  );
+};
+
+export default SliderScreen;
