cf5aa078aa1f652d09e29be701063ec411057a8f
thedeji <emmi2110@gmail.com>
2021-11-08 12:17:02 +0100

    sign up check box fixed

--- src/screens/Authentication/Registration/index.js
+++ src/screens/Authentication/Registration/index.js
@@ -118,19 +118,6 @@ export default function Register({ navigation }) {
                   setFormValues={setFormValues}
                 />
               </View>
-              <View style={{ paddingVertical: 20 }}>
-                <Button
-                  status="danger"
-                  size="large"
-                  accessibilityLiveRegion="assertive"
-                  accessibilityComponentType="button"
-                  accessibilityLabel="Continue"
-                  onPress={validateEmail}
-                  disabled={isLoading || !checked}
-                >
-                  <Text status="control">{t('continue')}</Text>
-                </Button>
-              </View>
             </View>
             <View>
               <View
@@ -148,7 +135,9 @@ export default function Register({ navigation }) {
                   Accept Terms and Conditions
                 </CheckBox>
 
-                <Text category="p2">{`${t('continueAgree')} woozeee's`}</Text>
+                <Text category="p2" style={{ marginVertical: 10 }}>{`${t(
+                  'continueAgree',
+                )} woozeee's`}</Text>
                 <Button
                   appearance="ghost"
                   size="large"
@@ -266,6 +255,19 @@ export default function Register({ navigation }) {
                 </Button>
               )}
             </View> */}
+              <View style={{ paddingVertical: 20 }}>
+                <Button
+                  status="danger"
+                  size="large"
+                  accessibilityLiveRegion="assertive"
+                  accessibilityComponentType="button"
+                  accessibilityLabel="Continue"
+                  onPress={validateEmail}
+                  disabled={isLoading || !checked}
+                >
+                  <Text status="control">{t('continue')}</Text>
+                </Button>
+              </View>
               <View
                 style={{
                   flexDirection: 'row',
